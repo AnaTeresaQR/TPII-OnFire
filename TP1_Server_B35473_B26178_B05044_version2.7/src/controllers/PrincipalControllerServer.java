9ff571ae4b0d38f0da5de1591ec241c6d6fbdf15
AnaTeresaQR <ana.quesada@ucrso.info>
2016-06-01 00:14:39 -0600

    Funciona la parte de crear subastas propias

---
+++ TP1_Server_B35473_B26178_B05044_version2.7/src/controllers/PrincipalControllerServer.java
@@ -0,0 +1,110 @@
+package controllers;
+
+import factorySale.ConcreteFactorySale;
+import factorySale.FactorySaleInterface;
+import java.io.IOException;
+import java.util.Calendar;
+import managementAdministrator.ManagementAdministrator;
+import models.SaleModel;
+import models.UserModel;
+import salesBuilder.AbstractBuilderCreateSale;
+import server.Server;
+import usersBuilder.ConcreteBuilderCreateUser;
+import usersBuilder.CustomException;
+import usersBuilder.Director;
+import views.AdministratorMenuView;
+import views.LoginAdministrator;
+import views.WelcomeServerView;
+
+/**
+ *
+ * @author Ana Teresa
+ */
+public class PrincipalControllerServer {
+
+    private Server server;
+    private boolean canLoginAdmin;
+    private UserModel modelUser;
+    private SaleModel saleModel;
+    private Director director;
+    private FactorySaleInterface factorySales;
+
+    private LoginAdministrator loginView;
+    private WelcomeServerView principalServer;
+    private AdministratorMenuView menuServer;
+
+    public PrincipalControllerServer(Server server) {
+        this.server = server;
+        director = new Director();
+        this.saleModel = new SaleModel();
+        this.modelUser = new UserModel();
+        loadFile();
+        loadFileSale();
+        factorySales = new ConcreteFactorySale();
+    }
+
+    public void NoConnection(PrincipalControllerServer controller) {
+        if (principalServer == null) {
+            principalServer = new WelcomeServerView(controller);
+        }
+        principalServer.setVisible(true);
+    }
+
+    public void processAdministrator(PrincipalControllerServer controller) throws IOException {
+        if (loginView == null) {
+            this.loginView = new LoginAdministrator(controller);
+        }
+        loginView.setVisible(true);
+    }
+
+    public synchronized boolean processLoginAdministrator(String admin, String password) {
+        ManagementAdministrator ma = new ManagementAdministrator();
+        this.canLoginAdmin = ma.login(admin, password);
+        return canLoginAdmin;
+    }
+
+    public PrincipalControllerServer() {
+        this.server = new Server();
+    }
+
+    public boolean createUser(String id, String name, String email, String password, Calendar birthdate, String phoneNumber) throws IOException, CustomException {
+        director.createUser(new ConcreteBuilderCreateUser(), modelUser, id, name, email, password, birthdate, phoneNumber);
+        boolean resultCreateModel = modelUser.createModel();
+        if (resultCreateModel) {
+            modelUser.save();
+        }
+        return resultCreateModel;
+    }
+
+    private void loadFile() {
+        modelUser.listLoader(); // update list
+    }
+
+    private void loadFileSale() {
+        saleModel.listLoader();
+    }
+
+    public UserModel loginUser(String email, String password) throws IOException {
+        UserModel temp = modelUser.login(email, password);
+        if (temp != null) {
+            modelUser = temp;
+        }
+        return temp;
+    }
+
+    public void processMenuAdministrator(PrincipalControllerServer controller) {
+        if (menuServer == null) {
+            menuServer = new AdministratorMenuView(controller);
+        }
+        menuServer.setVisible(true);
+    }
+
+    public boolean createSaleBuilder(AbstractBuilderCreateSale abstractBuilderSale, String brand, String model, int year, String carId, String color, String description, int days, int minOffer) throws IOException, CustomException {
+        factorySales.createSale(abstractBuilderSale, saleModel, modelUser, brand, model, year, carId, color, description, days, minOffer);
+        boolean resultCreateModel = saleModel.createModel();
+        if (resultCreateModel) {
+            saleModel.save();
+        }
+        return resultCreateModel;
+    }
+}
