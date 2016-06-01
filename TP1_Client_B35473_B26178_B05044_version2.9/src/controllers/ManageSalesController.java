b138d1e2a412f56b79403e55b30aa88b748a2083
AnaTeresaQR <ana.quesada@ucrso.info>
2016-06-01 14:03:00 -0600

    Se hace el proceso de ventanas del administrador

---
+++ TP1_Client_B35473_B26178_B05044_version2.9/src/controllers/ManageSalesController.java
@@ -0,0 +1,52 @@
+package controllers;
+
+import java.io.IOException;
+import javax.swing.JFrame;
+import socketClient.Client;
+import views.RegisterSaleView;
+import views.ShowOwnSalesView;
+
+/**
+ *
+ * @author Ana Teresa
+ */
+public class ManageSalesController {
+
+    private Client client;
+    private ShowOwnSalesView showOwnView;
+
+    public ManageSalesController(Client client) {
+        this.client = client;
+    }
+
+    public boolean createSale(String brand, String model, int year, String carId, String color, String description, int days, long minOffer, int typeSale) throws IOException {
+        return client.processRegisterSale(brand, model, year, carId, color, description, days, minOffer, typeSale);
+    }
+
+    public String messageRegisterSale(boolean canRegister) {
+        String messageStatus = "";
+        if (canRegister) {
+            messageStatus += "Se pudo registrar correctamente la subasta";
+        } else {
+            messageStatus += "No es posible registrar la subasta";
+        }
+        return messageStatus;
+    }
+
+    public void selectActionAddSale(int num) throws IOException {
+        client.processSelectAction(num);
+    }
+
+    public void returnView(PrincipalController principalController, JFrame child) {
+        child.dispose();
+        if (showOwnView == null) {
+            showOwnView = new ShowOwnSalesView(principalController, this);
+        }
+        showOwnView.setVisible(true);
+    }
+
+    public void processConfirmationAction(Boolean confirmation) throws IOException {
+        client.processConfirmationAction(confirmation);
+    }
+
+}
---
+++ TP1_Client_B35473_B26178_B05044_version2.9/src/socketClient/Client.java
@@ -0,0 +1,147 @@
+package socketClient;
+
+import controllers.ManageSalesController;
+import controllers.PrincipalController;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Calendar;
+
+public class Client {
+
+    private DataInputStream input;
+    private DataOutputStream output;
+
+    private ObjectInputStream inputObj;
+    private ObjectOutputStream outputObj;
+
+    private Socket client;
+    private final String HOST = "localhost";
+    private final int PORT = 8080;
+
+    private PrincipalController controller;
+    private ManageSalesController controllerSales;
+
+    public Client() {
+        controllerSales = new ManageSalesController(this);
+        controller = new PrincipalController(this, controllerSales);
+
+    }
+
+    public void runClient() {
+        try {
+
+            connectToServer();
+            getStreams();
+            controller.processConnection();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void connectToServer() throws IOException {
+        System.out.println("Attempting connection\n");
+        client = new Socket(HOST, PORT);
+        System.out.println("Connected to: " + client.getInetAddress().getHostName());
+    }
+
+    private void getStreams() throws IOException {
+        output = new DataOutputStream(client.getOutputStream());
+        output.flush();
+        input = new DataInputStream(client.getInputStream());
+
+        outputObj = new ObjectOutputStream(client.getOutputStream());
+        outputObj.flush();
+        inputObj = new ObjectInputStream(client.getInputStream());
+
+    }
+
+    public void closeStream() {
+        try {
+            output.close();
+            input.close();
+            outputObj.close();
+            inputObj.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void closeConnection() {
+        System.out.println("\nClosing connection");
+        try {
+            client.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public boolean processRegisterUser(String id, String name, String email, String password, Calendar birthdate, String phoneNumber) {
+        boolean status = false;
+        try {
+
+            output.writeUTF(id);
+            output.writeUTF(name);
+            output.writeUTF(email);
+            output.writeUTF(password);
+            outputObj.writeObject(birthdate);
+            output.writeUTF(phoneNumber);
+            status = input.readBoolean();
+            return status;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+
+    public void processSelectAction(int num) throws IOException {
+        output.writeInt(num);
+    }
+
+    public void processConfirmationAction(Boolean confirmation) throws IOException {
+        output.writeBoolean(confirmation);
+    }
+
+    public int processLoginUser(String email, String password) {
+        try {
+
+            output.writeUTF(email);
+            output.writeUTF(password);
+
+            return input.readInt();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public boolean processRegisterSale(String brand, String model, int year, String carId, String color, String description, int days, long minOffer, int typeSale) {
+        boolean status = false;
+        try {
+            output.writeUTF(brand);
+            output.writeUTF(model);
+            output.writeInt(year);
+            output.writeUTF(carId);
+            output.writeUTF(color);
+            output.writeUTF(description);
+            output.writeInt(days);
+            output.writeLong(minOffer);
+            output.writeInt(typeSale);
+            status = input.readBoolean();
+            return status;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+
+    public static void main(String[] args) {
+        new Client().runClient();
+    }
+
+}
